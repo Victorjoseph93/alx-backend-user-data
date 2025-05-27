f34f2e906b083bf981733fd9fdccee6a2f388473
Victorjoseph93 <you@example.com>
2025-05-27 10:55:06 +0000

    new updates

--- 0x01-Basic_authentication/models/base.py
+++ 0x01-Basic_authentication/models/base.py
@@ -73,4 +73,65 @@ class Base():
                 DATA[s_class][obj_id] = cls(**obj_json)
 
     @classmethod
+    def save_to_file(cls):
+        """ Save all objects to file
+        """
+        s_class = cls.__name__
+        file_path = ".db_{}.json".format(s_class)
+        objs_json = {}
+        for obj_id, obj in DATA[s_class].items():
+            objs_json[obj_id] = obj.to_json(True)
 
+        with open(file_path, 'w') as f:
+            json.dump(objs_json, f)
+
+    def save(self):
+        """ Save current object
+        """
+        s_class = self.__class__.__name__
+        self.updated_at = datetime.utcnow()
+        DATA[s_class][self.id] = self
+        self.__class__.save_to_file()
+
+    def remove(self):
+        """ Remove object
+        """
+        s_class = self.__class__.__name__
+        if DATA[s_class].get(self.id) is not None:
+            del DATA[s_class][self.id]
+            self.__class__.save_to_file()
+
+    @classmethod
+    def count(cls) -> int:
+        """ Count all objects
+        """
+        s_class = cls.__name__
+        return len(DATA[s_class].keys())
+
+    @classmethod
+    def all(cls) -> Iterable[TypeVar('Base')]:
+        """ Return all objects
+        """
+        return cls.search()
+
+    @classmethod
+    def get(cls, id: str) -> TypeVar('Base'):
+        """ Return one object by ID
+        """
+        s_class = cls.__name__
+        return DATA[s_class].get(id)
+
+    @classmethod
+    def search(cls, attributes: dict = {}) -> List[TypeVar('Base')]:
+        """ Search all objects with matching attributes
+        """
+        s_class = cls.__name__
+        def _search(obj):
+            if len(attributes) == 0:
+                return True
+            for k, v in attributes.items():
+                if (getattr(obj, k) != v):
+                    return False
+            return True
+        
+        return list(filter(_search, DATA[s_class].values()))
